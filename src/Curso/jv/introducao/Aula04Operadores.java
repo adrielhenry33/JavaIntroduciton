b2ce80a7b1b0d882bea93826e956c52fc594a5b3
adrielhenry33 <adrielhenrydb@gmail.com>
2024-08-01 19:22:07 -0300

    Operadores Relacionais

--- src/Curso/jv/introducao/Aula04Operadores.java
+++ src/Curso/jv/introducao/Aula04Operadores.java
@@ -2,6 +2,23 @@ package Curso.jv.introducao;
 
 public class Aula04Operadores {
     public static void main(String []args){
+        // - + / *
+        int numero1 = 10;
+        int numero2 = 20;
+        double resultado = (double) numero1 / (double) numero2;
+        System.out.println(numero1+numero2);
+        System.out.println(resultado);
+
+        // %
+        int resto  = 21%7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezmaiorQueVinte = 10>20;
+        boolean isDezmenorQueVinte = 10<20;
+        System.out.println(isDezmaiorQueVinte);
+        System.out.println(isDezmenorQueVinte);
+
 
     }
 }
