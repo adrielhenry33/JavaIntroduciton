534ae561b02bc7ad57502d0dbb96c8165215f627
adrielhenry33 <adrielhenrydb@gmail.com>
2024-08-04 19:13:56 -0300

    Exercicio estrutura de repeticao Aula 06

---
+++ src/Curso/jv/introducao/ExercicioAula06EstruturasDeRepeticao.java
@@ -0,0 +1,12 @@
+package Curso.jv.introducao;
+
+public class ExercicioAula06EstruturasDeRepeticao {
+    public static void main(String[] args) {
+
+        for (long i = 0; i <=1000000; i++){
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
