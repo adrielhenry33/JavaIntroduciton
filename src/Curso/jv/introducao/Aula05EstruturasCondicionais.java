0bdd8bc130d0658261a64a49dc7e3ddf7b5fa947
adrielhenry33 <adrielhenrydb@gmail.com>
2024-08-04 14:05:02 -0300

    Operadores condicionais Aula 05 parte 2

--- src/Curso/jv/introducao/Aula05EstruturasCondicionais.java
+++
@@ -1,9 +0,0 @@
-package Curso.jv.introducao;
-
-public class Aula05EstruturasCondicionais {
-    public static void main(String[] args) {
-        if(true){
-            System.out.println("Dentro do if");
-        }
-    }
-}
---
+++ src/Curso/jv/introducao/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,15 @@
+package Curso.jv.introducao;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args) {
+       int idade = 24;
+       String categoria;
+       if (idade == 25){
+           System.out.println("Idade igual a 25 anos ");
+       } else if (idade == 24) {
+           System.out.println("Idade igual a 24 anos ");
+       } else {
+           System.out.println("Idade e diferente de todas");
+       }
+    }
+}
