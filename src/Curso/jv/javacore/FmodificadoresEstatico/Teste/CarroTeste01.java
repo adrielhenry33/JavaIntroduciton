3d91aa374332bf16286402129f2a95fcc4fffc84
adrielhenry33 <adrielhenrydb@gmail.com>
2024-08-10 21:08:16 -0300

    Modificadores de acesso static fazem com que o atributo pertenca a classe

---
+++ src/Curso/jv/javacore/FmodificadoresEstatico/Teste/CarroTeste01.java
@@ -0,0 +1,18 @@
+package Curso.jv.javacore.FmodificadoresEstatico.Teste;
+
+import Curso.jv.javacore.FmodificadoresEstatico.Dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro("Porshe", 300);
+        Carro carro1 = new Carro("Mercedes" , 320);
+        Carro carro2 = new Carro("Lamborghini", 400);
+
+        Carro.velocidadeLimite = 180;
+
+        carro.imprime();
+        carro1.imprime();
+        carro2.imprime();
+
+    }
+}
