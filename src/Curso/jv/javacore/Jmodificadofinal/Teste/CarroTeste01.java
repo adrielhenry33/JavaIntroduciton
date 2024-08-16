3d893d49fb59d94a55fcd75dc592d2900768caff
adrielhenry33 <adrielhenrydb@gmail.com>
2024-08-16 20:48:28 -0300

    Modificador de acesso final tipos primitivos

---
+++ src/Curso/jv/javacore/Jmodificadofinal/Teste/CarroTeste01.java
@@ -0,0 +1,10 @@
+package Curso.jv.javacore.Jmodificadofinal.Teste;
+
+import Curso.jv.javacore.Jmodificadofinal.Dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
